320513cf74a81b1fd3dc59234ed9b53c43156d00
kinko7 <kion7kion@gmail.com>
2022-06-27 17:31:47 -0300

    Change res name variable for conflict

--- api/src/controllers/types.js
+++ api/src/controllers/types.js
@@ -2,20 +2,6 @@ const { Types } = require("../db.js");
 const axios = require("axios");
 const { uuid: v4 } = require("uuidv4");
 
-// typeId=0
-// types= [
-//     { name: 'normal',
-// id:typeId },   { name: 'fighting' },
-//     { name: 'flying' },   { name: 'poison' },
-//     { name: 'ground' },   { name: 'rock' },
-//     { name: 'bug' },      { name: 'ghost' },
-//     { name: 'steel' },    { name: 'fire' },
-//     { name: 'water' },    { name: 'grass' },
-//     { name: 'electric' }, { name: 'psychic' },
-//     { name: 'ice' },      { name: 'dragon' },
-//     { name: 'dark' },     { name: 'fairy' },
-//     { name: 'unknown' },  { name: 'shadow' }
-//   ]
 
 const getTypes = async (req, res) => {
   try {
